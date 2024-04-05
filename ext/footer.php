44cc321cd6e3ce26bac297df495e2aa34e54bea9
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-05 20:18:02 -0300

    Correções página Inicial

--- ext/footer.php
+++ ext/footer.php
@@ -2,10 +2,10 @@
     <div class="container">
       <div class="row">
         <div class="span12"> 
-		&copy; 2018 <a href="javascript:void(0)" onclick="owlphinhome()">Bacsyd</a>.
-		<span class="strict-mobile-no-visibility">|  <a href="javascript:void(0)" onclick="contact()">Contact us</a>
-		|  <a href="javascript:void(0)" onclick="help()">Help</a>
-		|  <a href="javascript:void(0)" onclick="admin()">Admin</a></span>
+		&copy; <?= date("Y"); ?> <a href="https://osidosos.com.br/" onclick="owlphinhome()">OsIdosos.com.br</a>
+		<span class="strict-mobile-no-visibility">|  <a href="javascript:void(0)" onclick="contact()">Fale Conosco</a>
+		|  <a href="javascript:void(0)" onclick="help()">Ajuda</a>
+		|  <a href="javascript:void(0)" onclick="admin()">Administração</a></span>
 		</div>
       </div>
     </div>
